ab0c64eb2a74691a9b9c82b99c21ae59bf16f6e4
pmtavares <pedrotavares41@gmail.com>
2017-12-02 16:49:08 +0000

    Configuration and First models

---
+++ Vega/Model/Modell.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vega.Model
+{
+    public class Modell
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Make Make { get; set; }
+
+        public int MakeId { get; set; }
+    }
+}
---
+++ Vega/Persistent/VegaDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Vega.Model;
+
+namespace Vega.Persistent
+{
+    public class VegaDbContext : DbContext
+    {
+        public VegaDbContext(DbContextOptions<VegaDbContext> options) : base(options)
+        {
+            //System.configuration.ConfigurationManager
+
+        }
+
+        public DbSet<Make> Makes { get; set; }
+        public DbSet<Modell> Modells { get; set; }
+     }
+}
